56b5f98466c6cf00ada39203f5d4992ce97954be
Mahsa472 <mahsa.amiri472@yahoo.com>
2026-02-22 20:47:16 +0100

    initial project

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+import time
+import requests
+from dotenv import load_dotenv
+from langchain.tools import tool
+from langchain.agents import create_agent
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
+
+
+
+
+def main():
+    print("Hello from agents-observation!")
+
+
+if __name__ == "__main__":
+    main()
